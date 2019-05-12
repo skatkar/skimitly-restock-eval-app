6b54a5e2202dafb1f8ee22dae8188ce7fef054b5
skatkar <skatkar225@gmail.com>
2019-05-12 08:14:40 -0700

    Initial commit

---
+++ src/main/java/com/skimitly/restock/main/RestockEvalApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.skimitly.restock.main;
+
+/**
+ * Entry point into the application
+ * @author Swapnil
+ *
+ */
+public class RestockEvalApp {
+
+	/**
+	 * This method will collect the necessary data, evaluates it and displays the result based on that
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/com/skimitly/restock/service/RestockEvalService.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.skimitly.restock.service;
+
+/**
+ * Service class - It serves various methods required by the main method for evaluation
+ * @author Swapnil
+ *
+ */
+public class RestockEvalService {
+
+}
